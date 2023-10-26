1d6ceee2982f8984740707afcdb4df7805b3ff70
nikborkowski <38554274+nikborkowski@users.noreply.github.com>
2023-10-26 14:11:07 +0300

    reshaped

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # amazingdebugger
+just *run main.go* and open browser on *:8000*
--- main.go
+++ main.go
@@ -8,10 +8,6 @@ import (
 	"strings"
 )
 
-type Film struct {
-	Title    string
-	Director string
-}
 type RuleSet struct {
 	Id     string
 	Expect string
@@ -31,40 +27,36 @@ var RuleSets []RuleSet = []RuleSet{
 func main() {
 	fmt.Println("Started on localhost:8000")
 
-	// initial handler with first-state html
-	root := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		initRuleSets := []RuleSet{}
-		rulesetInit := map[string][]RuleSet{
-			"RuleSets": initRuleSets,
-		}
-		tmpl.Execute(w, rulesetInit)
-	}
-
-	// ruleset function handler
-	matchSignalToRuleSets := func(w http.ResponseWriter, r *http.Request) {
-		//mock longing of load - for spinner check
-		//time.Sleep(1 * time.Second)
-		signalId := r.PostFormValue("signalid")
-
-		matchedRS := []RuleSet{}
-		for _, rs := range RuleSets {
-			if strings.Contains(signalId, rs.Expect) {
-				matchedRS = append(matchedRS, rs)
-			}
-		}
-		fmt.Println("Matched RS:", matchedRS)
-		matchedRuleSetsBlock := map[string][]RuleSet{
-			"RuleSets": matchedRS,
-		}
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "rulesets-list-element", matchedRuleSetsBlock)
-	}
-
 	//hander function vs resource
 	http.HandleFunc("/", root)
 	http.HandleFunc("/rule-sets/", matchSignalToRuleSets)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	initRuleSets := []RuleSet{}
+	rulesetInit := map[string][]RuleSet{
+		"RuleSets": initRuleSets,
+	}
+	tmpl.Execute(w, rulesetInit)
+}
+
+func matchSignalToRuleSets(w http.ResponseWriter, r *http.Request) {
+	signalId := r.PostFormValue("signalid")
+
+	matchedRS := []RuleSet{}
+	for _, rs := range RuleSets {
+		if strings.Contains(signalId, rs.Expect) {
+			matchedRS = append(matchedRS, rs)
+		}
+	}
+	fmt.Println("Matched RS:", matchedRS)
+	matchedRuleSetsBlock := map[string][]RuleSet{
+		"RuleSets": matchedRS,
+	}
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.ExecuteTemplate(w, "rulesets-list-element", matchedRuleSetsBlock)
 }
